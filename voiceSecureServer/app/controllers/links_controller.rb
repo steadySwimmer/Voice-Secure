5ad17c7dd15b3e93d3d228ec4b61d51a8972e94d
Vlad Podilnyk <vlad.podilnyk@gmail.com>
2016-12-04 00:11:22 +0200

    Add server part

---
+++ voiceSecureServer/app/controllers/links_controller.rb
@@ -0,0 +1,87 @@
+class LinksController < ApplicationController
+  before_action :set_link, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, except: [:index, :show]
+
+  # GET /links
+  # GET /links.json
+  def index
+    @links = Link.all
+  end
+
+  # GET /links/1
+  # GET /links/1.json
+  def show
+  end
+
+  # GET /links/new
+  def new
+    @link = current_user.links.build
+  end
+
+  # GET /links/1/edit
+  def edit
+  end
+
+  # POST /links
+  # POST /links.json
+  def create
+    @link = current_user.links.build(link_params)
+
+    respond_to do |format|
+      if @link.save
+        format.html { redirect_to @link, notice: 'Link was successfully created.' }
+        format.json { render :show, status: :created, location: @link }
+      else
+        format.html { render :new }
+        format.json { render json: @link.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /links/1
+  # PATCH/PUT /links/1.json
+  def update
+    respond_to do |format|
+      if @link.update(link_params)
+        format.html { redirect_to @link, notice: 'Link was successfully updated.' }
+        format.json { render :show, status: :ok, location: @link }
+      else
+        format.html { render :edit }
+        format.json { render json: @link.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /links/1
+  # DELETE /links/1.json
+  def destroy
+    @link.destroy
+    respond_to do |format|
+      format.html { redirect_to links_url, notice: 'Link was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  def upvote
+    @link = Link.find(params[:id])
+    @link.upvote_by current_user
+    redirect_to :back
+  end
+
+  def downvote
+    @link = Link.find(params[:id])
+    @link.downvote_from current_user
+    redirect_to :back
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_link
+      @link = Link.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def link_params
+      params.require(:link).permit(:title, :url)
+    end
+end
---
+++ voiceSecureServer/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  resources :comments
+  devise_for :users
+  resources :links do
+    member do
+      put "like", to: "links#upvote"
+      put "dislike", to: "links#downvote"
+    end
+    resources :comments
+  end
+  root to: "links#index"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end

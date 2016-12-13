548b3ee5f67f176dbed2e1d8a27e1701d2af9699
Vlad Podilnyk <vlad.podilnyk@gmail.com>
2016-12-14 01:04:55 +0200

    Update application_controller.rb

--- voiceSecure/app/controllers/application_controller.rb
+++ voiceSecure/app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
-	before_filter :configure_permitted_parameters, if: :devise_controller?
+	#before_filter :configure_permitted_parameters, if: :devise_controller?
 
-	protected
+	#protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:account_update) << :name
-  end
+  #def configure_permitted_parameters
+   # devise_parameter_sanitizer.for(:sign_up) << :name
+   # devise_parameter_sanitizer.for(:account_update) << :name
+  #end
 end

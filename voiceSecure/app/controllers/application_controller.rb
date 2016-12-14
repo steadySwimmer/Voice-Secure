8b563ed96fa302e19b1c51766e4b98350d009675
Vlad Podilnyk <vlad.podilnyk@gmail.com>
2016-12-15 01:25:40 +0200

    Make pretty views

--- voiceSecure/app/controllers/application_controller.rb
+++ voiceSecure/app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
-	#before_filter :configure_permitted_parameters, if: :devise_controller?
+	before_filter :configure_permitted_parameters, if: :devise_controller?
 
-	#protected
+	protected
 
-  #def configure_permitted_parameters
-   # devise_parameter_sanitizer.for(:sign_up) << :name
-   # devise_parameter_sanitizer.for(:account_update) << :name
-  #end
+  def configure_permitted_parameters
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+   devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
+  end
 end
